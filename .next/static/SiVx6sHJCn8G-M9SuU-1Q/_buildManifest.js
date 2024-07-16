33f4dab63e2b8d9e245f5d63bafca4584b9503e6
JEM CDYN <cdynjm@gmail.com>
2024-07-16 18:08:46 +0800

    'u'

---
+++ .next/static/SiVx6sHJCn8G-M9SuU-1Q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/5f88011d96ca9c55.css","static/chunks/pages/index-b39a2f5cc245f1db.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
